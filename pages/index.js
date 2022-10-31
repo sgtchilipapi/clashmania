c3176824654cd349b8ed53a422c99eac3df02f62
sgtchilipapi <jayemmanuelg@gmail.com>
2022-11-01 05:54:00 +0800

    build: updated contracts repo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// import { ethers } from 'hardhat'
 
 import React from 'react'
 import * as apis from "../random-clash-contracts/api/contracts/contracts-api"
@@ -8,9 +9,16 @@ import * as apis from "../random-clash-contracts/api/contracts/contracts-api"
 export default function Home() {
 
   async function getCharacterProps(){
-    console.log(await apis.core.equipments.transfer("0x6b4787A1A2aE3C31aeA3b24C36cFcFb390D946B6", "0x6b4787A1A2aE3C31aeA3b24C36cFcFb390D946B6", 1))
+    //console.log(await apis.core.equipments.transfer("0x6b4787A1A2aE3C31aeA3b24C36cFcFb390D946B6", "0x6b4787A1A2aE3C31aeA3b24C36cFcFb390D946B6", 1))
     // console.log(await apis.core.equipments.getEquipmentStats(1))
     // console.log(await apis.core.equipments.getEquipmentProperties(1))
+    // await apis.periphery.eqpt_mngr.unequipAll(1)
+    // console.log(`Equipped items: ${await apis.periphery.eqpt_mngr.getCharacterEquipments(1)}`)
+    await apis.periphery.routers.ctr_minter.requestCharacter(0,"gorogo","0.01")
+  }
+
+  async function mintCharacter(){
+    await apis.periphery.routers.ctr_minter.mintCharacter()
   }
 
   return (
@@ -24,6 +32,9 @@ export default function Home() {
       <main>
         <div>
           <button onClick={getCharacterProps}>Check RPC</button>
+        </div>
+        <div>
+          <button onClick={mintCharacter}>mint</button>
         </div>
       </main>
       <footer>
