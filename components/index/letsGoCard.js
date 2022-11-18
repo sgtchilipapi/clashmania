3b795a67b98c4ca91ded15d3fb86d951ee40c14f
sgtchilipapi <jayemmanuelg@gmail.com>
2022-11-18 16:54:11 +0800

    feat: add home page welcome message

---
+++ components/index/letsGoCard.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Image from 'next/image'
+import Link from 'next/link'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, Grid } from '@mui/material';
+
+export default function LetsGoCard(props) {
+  return (
+    <Card>
+      <Typography gutterBottom variant="h6" color="primary.main" sx={{ mt: 1 }}>
+        WELCOME TO RANDOM CLASH!
+      </Typography>
+      <Grid container>
+        <Grid item container xs={6} style={{transform: 'scaleX(-1)'}} justifyContent='left'>
+          <Image
+            height="140"
+            width="140"
+            src="/images/characters/0 Viking/Amazed.png"
+          />
+        </Grid>
+        <Grid item container xs={6} justifyContent='left'>
+          <Image
+            height="140"
+            width="140"
+            src="/images/characters/2 Troll/Amazed.png"
+          />
+        </Grid>
+      </Grid>
+
+
+      <CardContent>
+        <Typography gutterBottom variant="h6" color="primary.main">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="primary.main">
+          {props.body}
+        </Typography>
+        <Link href='/characters'><Button variant='outlined' sx={{ mt: 1 }}>Lets Go!</Button></Link>
+      </CardContent>
+    </Card>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,47 @@
 import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import Router from 'next/router'
 import styles from '../styles/Home.module.css'
 import { useAccount } from 'wagmi'
+import CssBaseline from '@mui/material/CssBaseline';
 
-import { Box } from '@mui/material'
+import { Box, Container, Typography, Button } from '@mui/material'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 import * as apis from "../random-clash-contracts/api/contracts/contracts-api"
+import LetsGoCard from '../components/index/letsGoCard'
 
 export default function Home(props) {
-  const {address} = useAccount()
+  const { address } = useAccount()
+  // const toCharacters = () => {Router.push('/characters')}
 
   return (
-    <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', color: 'primary.main' }}>
-    <div>
-      <Head>
-        <title >Random Clash App</title>
-        <meta name="description" content="A complete on-chain Role-Playing-Game with character progression, item & equipment crafting, integral Defi economy and a breath-taking battle system." />
-        {/* <link rel="icon" href="/rc-icon.ico" /> */}
-      </Head>
-      <main>
-        {/* Home Image */}
-        {/* Welcome, Clasher! */}
-        {/* Get Started Button */}
 
-        {/* <WalletAuth /> */}
-       
-      </main>
-    </div>
-    </Box>
+    <React.Fragment>
+      <CssBaseline />
+      <div>
+        <Head>
+          <title >Random Clash App</title>
+          <meta name="description" content="A complete on-chain Role-Playing-Game with character progression, item & equipment crafting, integral Defi economy and a breath-taking battle system." />
+          {/* <link rel="icon" href="/rc-icon.ico" /> */}
+        </Head>
+        <main>
+          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', color: 'primary.main' }}>
+            <Container fixed justify="center" align="center" maxWidth='xs'>
+              <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', color: 'primary.main', pt:2 }}>
+                <LetsGoCard 
+                  title={'This game is free to play!'}
+                  body={`To start or resume your clashing journey, click the button below.`}
+                />
+              </Box>
+            </Container>
+
+          </Box>
+        </main>
+      </div >
+    </React.Fragment >
   )
 }
