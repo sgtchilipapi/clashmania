8b9a78b3f5446c43cc0c4e8c383a79c783be33c2
sgtchilipapi <jayemmanuelg@gmail.com>
2023-02-12 11:17:33 +0800

    fix: fix what button to display in the catalyst items in wallet page

--- components/wallet/tokenListItem.js
+++ components/wallet/tokenListItem.js
@@ -34,7 +34,7 @@ export default function TokenListItem(props) {
             props.setIsLoading(true)
             let balance = await c_apis.core.tokens.balanceOf(tokens_lib.getUnderlyingLP(props.token_name), address)
             setUnderlyingLpBalance(balance)
-            let _approvedLP = await c_apis.core.tokens.allowance(tokens_lib.getUnderlyingLP(props.token_name),address, props.token_name)
+            let _approvedLP = await c_apis.core.tokens.allowance(tokens_lib.getUnderlyingLP(props.token_name), address, props.token_name)
             setApprovedLP(_approvedLP)
             props.setIsLoading(false)
         }
@@ -54,14 +54,21 @@ export default function TokenListItem(props) {
         props.setIsLoading(false)
     }
 
+    const getCatalystAction = () => {
+        if(parseInt(underlyingLpBalance) >= 10) {
+            if(parseInt(approvedLP) < 10){
+                return (<Button onClick={handleMintCatalyst}>{`Approve ${tokens_lib.getTokenTickerByName(tokens_lib.getUnderlyingLP(props.token_name))}`}</Button>)
+            }else{
+                return (<Button onClick={handleMintCatalyst}>{`Mint`}</Button>)
+            }
+        }else{
+            return(<a target="_blank" rel="noreferrer" href={tokens_lib.getSpookyAddress(props.token_name)}><Button>{`Get ${tokens_lib.getTokenTickerByName(tokens_lib.getUnderlyingLP(props.token_name))} LP`}</Button></a>)
+        }
+    }
+
     const actionButton = (
         (props.token_name == "yellowspark" || props.token_name == "whitespark" || props.token_name == "redspark" || props.token_name == "bluespark") ?
-                parseInt(approvedLP) < 10 ? 
-                <Button onClick={handleMintCatalyst}>{`Approve ${tokens_lib.getTokenTickerByName(props.token_name)}`}</Button> :
-                    parseInt(underlyingLpBalance) >= 10 ?
-                    <Button onClick={handleMintCatalyst}>{`Mint ${tokens_lib.getTokenTickerByName(props.token_name)}`}</Button> :
-                <a target="_blank" rel="noreferrer" href={tokens_lib.getSpookyAddress(props.token_name)}><Button>{`Get ${tokens_lib.getTokenTickerByName(tokens_lib.getUnderlyingLP(props.token_name))} LP`}</Button></a> :
-            ""
+            getCatalystAction() : ""
     )
 
     return (
@@ -76,7 +83,7 @@ export default function TokenListItem(props) {
                     />
                 </ListItemAvatar>
                 <ListItemText id={props.token_name} primary={`${tokenBalance}`} secondary={`${tokens_lib.getTokenTickerByName(props.token_name)}`} />
-                
+
                 {actionButton}
             </ListItemButton>
         </ListItem>
